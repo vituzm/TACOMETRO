524d2f759343ea48e9011e7e854d38e65a893479
vituzm <marqueszenivitor@gmail.com>
2024-06-22 13:47:56 -0300

    teste

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,7 +7,7 @@
   * @attention
   *
   * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
+  * All rights reserved. 
   *
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
